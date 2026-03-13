d606ed57245deaa2c5e28f80eea9cd39920e0599
J <j@gmail.com>
2026-03-13 10:45:09 +0100

    feat: login route

--- integration_hub-backend/routes/auth.routes.js
+++ integration_hub-backend/routes/auth.routes.js
@@ -4,11 +4,11 @@ import validate from "../middlewares/validate.middleware.js";
 import { signUpSchema, logInSchema } from "../validation/auth.schemas.js";
 
 import signUp from "../services/auth/signUp.js";
+import logIn from "../services/auth/logIn.js";
 
 const authRouter = Router();
 
 authRouter.post('/signup', validate(signUpSchema), signUp);
-authRouter.post('/login', validate(logInSchema), (req, res) => res.send('Login route'));
-authRouter.post('/logout', (req, res) => res.send('Logout route'));
+authRouter.post('/login', validate(logInSchema), logIn);
 
 export default authRouter;
--- integration_hub-backend/services/auth/logIn.js
+++ integration_hub-backend/services/auth/logIn.js
@@ -1,36 +1,38 @@
-import User from "../../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
+import User from "../../models/user.model.js";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../../config/env.js";
 
 const logIn = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
+    // Check if an user with this email exists
     const user = await User.findOne({ email });
 
     if (!user) {
       const error = new Error("Invalid credentials");
-      error.statusCode = 409;
+      error.statusCode = 401;
       throw error;
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if (hashedPassword !== user.password) {
+    if (!isPasswordValid) {
       const error = new Error("Invalid credentials");
-      error.statusCode = 409; //TODO What is this code?
+      error.statusCode = 401;
       throw error;
     }
 
-    const token = jwt.sign({ userId: users[0]._id }, JWT_SECRET, {
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
       expiresIn: JWT_EXPIRES_IN,
     });
 
     res.status(200).json({
       success: true,
       message: "User authenticated successfully",
-      data: { token },
+      data: { token, user: { _id: user._id, email: user.email } },
     });
   } catch (error) {
     next(error);
