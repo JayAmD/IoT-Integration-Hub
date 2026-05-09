ad4efd62db71c69d0513e8e51de79c35b6425bf2
J <j@gmail.com>
2026-05-09 12:36:02 +0200

    fix(BE, validate middleware): not only validate but stripping out other filed not in zod schema

--- integration_hub-backend/middlewares/validate.middleware.js
+++ integration_hub-backend/middlewares/validate.middleware.js
@@ -1,10 +1,16 @@
 const validate = (schema) => (req, res, next) => {
   try {
-    schema.parse({
+    const validated = schema.parse({
       body: req.body,
       params: req.params,
       query: req.query,
     });
+
+    // Overwrite the request objects with the "cleaned" versions from Zod
+    req.body = validated.body;
+    req.params = validated.params;
+    req.query = validated.query;
+
     next();
   } catch (error) {
     next(error);
